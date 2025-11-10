5ce26f42dda7092f24a44781db634037e396e672
WhiteFrostune <3566621734@qq.com>
2025-11-10 20:20:27 +0800

    初始提交：创建贪吃蛇游戏项目

---
+++ Snake Project/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int i = 0;
+	cin >> i;
+	if (i < 6) {
+		cout << "ture" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
+	return 0;
+}
